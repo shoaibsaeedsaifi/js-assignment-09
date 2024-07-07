8f19879ca8de7494072ee6484f026dc7fc43e7b8
shoaibsaeedsaifi <riazfffkhan@gmail.com>
2024-07-07 11:46:10 +0500

    ok done

---
+++ script.js
@@ -0,0 +1,64 @@
+// Problem no 01:
+
+// solution:
+
+//get a number from a user
+// let number = +prompt("Enter a number.");
+//print the next 10 numbers
+// for (let i = 0; i <= 10; i++) {
+    // console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+
+// problem no 2
+
+//solution:
+
+// let userInput = [];
+// for (let i = 0; i < 5; i++) {
+// let number = +prompt("Enter a number:");
+// userInput.push(number)
+// }
+// //print the sum of these number:
+// let sum = 0;
+// for (let i = 0; i < userInput.length; i++) {
+//     sum += userInput[i];
+// }
+// console.log("the sum of array is " + sum)
+
+// Problem no 03:
+
+//solution:
+
+// find highest digit of the array  
+// let userInput= [];
+// for (let i = 0; i < 5; i++) {
+//     let number = +prompt("Enter a number")
+//     userInput.push(number);
+// }
+// let largest = userInput[0];
+// for (let i = 1; i < userInput.length; i++) {
+//     if (userInput[i] > largest) {
+//         largest = userInput[i];
+//     }
+// }
+// // Print the largest number:
+// console.log("The largest no of the array is: " + largest);
+
+
+// Problem no 04:
+
+ // solution
+
+// let userInput = prompt("Enter a string:");
+
+// let reverseName = "";
+// for (let i = userInput.length - 1; i >= 0; i--) {
+//     reverseName = reverseName + userInput[i];
+// }
+
+// console.log("Your Reverse name is " + reverseName)
+
+// Problem no 05:
+ 
+// solution:
